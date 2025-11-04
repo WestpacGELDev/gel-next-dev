a3afb5a070b0a62c6ece421d45a06fe1a07e25c2
Jeremy Ortiz <jeremy.ortiz@westpac.com.au>
2025-11-04 16:16:16 +1100

    feat: update keystatic config

--- apps/site-dev/keystatic.config.ts
+++ apps/site-dev/keystatic.config.ts
@@ -1,20 +1,42 @@
-import { config, collection, fields } from '@keystatic/core';
+import {
+  config,
+  collection,
+  fields,
+  LocalConfig,
+  GitHubConfig,
+} from "@keystatic/core";
+
+const IS_VERCEL_BUILD =
+  typeof process.env.NEXT_PUBLIC_GIT_REPO_OWNER === "string" &&
+  process.env.NEXT_PUBLIC_GIT_REPO_OWNER !== "";
 
 export const markdocConfig = fields.markdoc.createMarkdocConfig({});
 
+const storage: LocalConfig["storage"] | GitHubConfig["storage"] =
+  IS_VERCEL_BUILD
+    ? {
+        kind: "github",
+        pathPrefix: "apps/site-dev",
+        repo: {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          owner: process.env.NEXT_PUBLIC_GIT_REPO_OWNER!,
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          name: process.env.NEXT_PUBLIC_GIT_REPO_SLUG!,
+        },
+      }
+    : { kind: "local" };
+
 export default config({
-  storage: {
-    kind: 'local',
-  },
+  storage,
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.markdoc({ label: 'Content' }),
+        title: fields.slug({ name: { label: "Title" } }),
+        content: fields.markdoc({ label: "Content" }),
       },
     }),
   },
